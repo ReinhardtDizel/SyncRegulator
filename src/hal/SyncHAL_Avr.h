4a66b0508007adfafc1edf0fa44891cb4ce7689b
Michael <dizel882@gmail.com>
2026-06-22 21:21:37 +0300

    refactoring

--- src/hal/SyncHAL_Avr.h
+++ src/hal/SyncHAL_Avr.h
@@ -14,58 +14,37 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-// Объявления функций, которые предоставляет пользователь (будут в main.cpp)
-extern "C" {
-    unsigned long millis();
-    void delay(unsigned long ms);
-    void delayMicroseconds(unsigned int us);
-}
-
-// Глобальные указатели на обработчики прерываний (определяются в main.cpp)
 extern void (*intFunc[2])();
 
-/**
- * @class SyncHAL_Avr
- * @brief Реализация SyncHAL для AVR с прямым доступом к регистрам
- */
 class SyncHAL_Avr : public SyncHAL {
 public:
     SyncHAL_Avr() {}
     ~SyncHAL_Avr() {}
 
-    // Настроить пин как вход с подтяжкой
     void pinModeInputPullup(uint8_t pin) override {
         volatile uint8_t* ddr = _getDdr(pin);
         volatile uint8_t* port = _getPort(pin);
         uint8_t bit = _getBit(pin);
         if (ddr && port) {
-            *ddr &= ~(1 << bit);   // вход
-            *port |= (1 << bit);   // подтяжка включена
+            *ddr &= ~(1 << bit);
+            *port |= (1 << bit);
         }
     }
 
-    // Прикрепить прерывание к пину (поддерживаем INT0 и INT1)
     void attachInterruptExt(uint8_t pin, void (*isr)(), int mode) override {
-        // Определяем, какой пин соответствует INT0 или INT1
-        // По умолчанию на ATmega1284P: INT0 = PD2 (пин 16), INT1 = PD3 (пин 17)
         uint8_t intNum = 0xFF;
-        if (pin == 16) intNum = 0;   // INT0
-        else if (pin == 17) intNum = 1; // INT1
-        else return; // неподдерживаемый пин
-
-        // Сохраняем указатель на функцию
+        if (pin == 16) intNum = 0;
+        else if (pin == 17) intNum = 1;
+        else return;
         if (intNum == 0) intFunc[0] = isr;
         else intFunc[1] = isr;
-
-        // Настраиваем режим срабатывания (без Arduino-макросов)
         uint8_t mask = 0;
         switch (mode) {
-            case 1:  mask = (1 << ISC01) | (1 << ISC00); break; // RISING
-            case 2:  mask = (1 << ISC01); break;               // FALLING
-            case 3:  mask = (1 << ISC00); break;               // CHANGE
-            default: mask = 0; break;                          // LOW (не используется)
+            case 1: mask = (1 << ISC01) | (1 << ISC00); break;
+            case 2: mask = (1 << ISC01); break;
+            case 3: mask = (1 << ISC00); break;
+            default: mask = 0; break;
         }
-
         if (intNum == 0) {
             EICRA = (EICRA & ~((1 << ISC01) | (1 << ISC00))) | mask;
             EIMSK |= (1 << INT0);
@@ -92,7 +71,6 @@ public:
     }
 
 private:
-    // Маппинг пинов (ваша нумерация)
     volatile uint8_t* _getDdr(uint8_t pin) {
         if (pin >= 1 && pin <= 8)   return &DDRB;
         if (pin >= 14 && pin <= 21) return &DDRD;
